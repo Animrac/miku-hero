025ec37a475b31a54d3c5d5dd41fbf4439cf9538
seburoh <seburoh@outlook.com>
2024-06-26 21:29:44 -0700

    better math

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ import { Player } from "textalive-app-api";
 class Main {
 
     constructor() {
+        this.step = 100; //How many ms make up one vertical slice (aka, a square)
+        this.blockSize = 40; //Horizontal space taken up by each vertical slice.
+
         //init canvas
         this.canvasInit();
 
@@ -26,29 +29,21 @@ class Main {
      * Initializes the viewable canvas properties.
      */
     canvasInit() {
-        this.canvas = document.getElementById("canvas");
-        // this.canvas.style.background = "#ECFFDC"; // color is called nyanza :3
-
-        //make the canvas take up the entire screen uwu
-        // this.canvas.width = window.innerWidth;
-        // this.canvas.height = window.innerHeight;
-
         this.canvasDiv = document.getElementById("canvasDiv");
-        this.canvas.width = this.canvasDiv.parentNode.clientWidth;
-        this.canvas.height = this.canvasDiv.parentNode.clientHeight;
-
+        this.canvas = document.getElementById("canvas");
         this.context = this.canvas.getContext("2d");
-        this.context.font = "40px PixelMplus10-Regular";
+        
+        // this.canvas.style.background = "#ECFFDC"; // color is called nyanza :3
+        
+        this.resizeCanvas();
     }
 
     /***
      * Resizes the canvas as the player changes the size of the window.
      */
     resizeCanvas() {
-        this.canvas = document.getElementById("canvas");
-        this.canvasDiv = document.getElementById("canvasDiv");
-        this.canvas.width = this.canvasDiv.parentNode.clientWidth;
-        this.canvas.height = this.canvasDiv.parentNode.clientHeight;
+        this.canvas.width = this.canvasDiv.clientWidth;
+        this.canvas.height = this.canvasDiv.clientHeight;
         this.context.font = "40px PixelMplus10-Regular";
     }
 
@@ -96,20 +91,18 @@ class Main {
         //     document.getElementById("playOverlay").style.display = "none"
         // }(this.player));
 
-        document.getElementById("playOverlay").addEventListener("click", () => function (p) {
-                p.requestPlay();
-                document.getElementById("playOverlay").style.display = "none"
-        }(this.player));
+        document.getElementById("playOverlay").addEventListener("click", () => {
+            this.player.requestPlay();
+            document.getElementById("playOverlay").style.display = "none";
+        });        
 
-        //currently set to click the canvas to pause music
-        document.getElementById("canvas").addEventListener("click", () => function (p) {
-            if (p.isPlaying) {
-                p.requestPause();
+        document.getElementById("canvas").addEventListener("click", () => {
+            if (this.player.isPlaying) {
+                this.player.requestPause();
             } else {
-                p.requestPlay();
+                this.player.requestPlay();
             }
-        }(this.player));
-
+        });
     }
 
     /***
@@ -143,7 +136,6 @@ class Main {
                 ly = ly.next;
             }
         }
-        console.log(lyrics);
         this.lyrics = lyrics;
     }
 
@@ -160,7 +152,7 @@ class Main {
     updateFrame() {
         //show lyrics
         if (this.player.isPlaying && this.timeStamp > 0) {
-            this.showlyric();
+            this.showLyric();
         }
 
         //may i have another?
@@ -168,50 +160,77 @@ class Main {
     }
 
     /**
-     * Shows lyrics and squares on canvas.
+     * Draws on canvas.
      */
-    showlyric() {
-        if (!this.lyrics) return; //Don't do this if no lyrics have been loaded.
-        let step = 100; //How many ms make up one vertical slice (aka, a square)
-        let blockSize = 40; //Horizontal space taken up by each vertical slice.
-        let lastBlockEnd = -1; //Comparison for right border of each given letter to prevent overlap.
+    showLyric() {
+        if (!this.lyrics) return; //Don't do stuff if no lyrics have been loaded.
+
         let lyricY = this.canvas.height - 120; //Vertical placement for lyric bar.
-        let blockBaseY = lyricY - 80; //Vertical placement for floor of where squares will go.
-        let canvasUsedPercent = 0.4; //How much of the canvas is used, from 0-1.
-
-        //Self-notes to remember how the math works.
-        //cw = 2000
-        //20 blocks (2000/100)
-        //last ly displayed: 2000 = ?/100 * 16 | 2000/16 * 100 | width/blockSize*step
-
-        //Start scrolling when math says to.
-        //Math: Current time - (utilized canvas width) / (block size) * (step)
-        let startStamp = this.timeStamp - ((this.canvas.width * canvasUsedPercent) / blockSize * step);
-        startStamp = Math.max(0, startStamp); //Default to 0 if we haven't used whole width yet
-
-        //Clear previous draw
-        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
-
-        //Draw rects
-        for (let i = 0; i < this.timeStamp; i += step) {
-            this.context.fillRect((i / step) * blockSize, blockBaseY, 39, 39); //Draw a square
+        let blockBaseY = lyricY //lyricY - 80; //Vertical placement for floor of where squares will go.
+        let delay = 200; //amount of time in ms to delay from impact
+        let startPixel = this.timeToPixel(this.timeStamp - delay); //Convert timestamp to pixel for cleaner math.
+        
+        //Determine first lyric that will be shown
+        let bufferedTime = this.timeStamp - delay - this.step; //Be generous in what lyric we grab first.
+        let lyi = this.lastLy ? this.lastLy : 0; //Grab previously known first lyric index to start from
+        
+        //Walk through remaining lyrics until a good start point is found.
+        while (lyi < this.lyrics.length && this.lyrics[lyi].startTime < bufferedTime) {
+            lyi++;
         }
+        this.lastLy = lyi; //Stash it's index for next time to be faster in search.
+        
+        this.context.clearRect(0,0, this.canvas.width, this.canvas.height); //Clear screen.
+        
+        //Determine x coordinate of start and end block.
+        let firstX = -(startPixel % this.blockSize);
+        let lastX = this.canvas.width + this.blockSize;
 
-        //Draw lyrics
-        for (let i = 0; i < this.lyrics.length; i++) {
-            let ly = this.lyrics[i]; //Get current lyric
+        //Holders for fadeouts and previous lyrics.
+        let prevLy = this.lyrics[lyi - 1];
+        let shading = 100;
 
-            //Lyrics are drawn when their timestamp is before the present, and after the left screen border
-            if (ly.startTime <= this.timeStamp && ly.startTime >= startStamp) {
-                let adjustedTime = ly.startTime - startStamp; //Adjust the draw position based on scroll
-                let xpos = (adjustedTime / step) * blockSize; //Adjust the draw position based on step/block size
-                //let xpos = (ly.startTime / step) * blockSize;
-                if (lastBlockEnd > xpos) xpos = lastBlockEnd + 1; //If char would overlap previous, bump it forward
-                lastBlockEnd = xpos + blockSize; //Set the right border to check for future overlap
-                
-                this.context.fillText(ly.text, xpos, lyricY); //Draw the lyric
+        //Step through each block and decide what to draw.
+        for (let i = firstX; i < lastX; i += this.blockSize) {
+            let ly = this.lyrics[lyi];
+
+            //Does this lyric exist and should it be displayed?
+            if (ly && this.timeToPixel(ly.startTime) <= (i + startPixel)) {
+                this.context.fillStyle = "black"; //Reset fade.
+                shading = 100;
+                this.context.fillText(ly.text, i, lyricY); //Draw the lyric.
+                prevLy = ly; //Stash previous for possible fadeout.
+                lyi++; //Move index.
+            } else if (prevLy && prevLy.text != '”' && this.timeToPixel(prevLy.endTime) > (i + startPixel + this.blockSize)) {
+                //Fade between 100 & 220.
+                this.context.fillStyle = `rgb(
+                    ${Math.min(shading, 220)}
+                    ${Math.min(shading, 220)}
+                    ${Math.min(shading, 220)})`;
+                this.context.fillText(prevLy.text, i, lyricY); //Draw the lyric
+                shading += 40; //Increase fade.
+
+            } else { //Show a block instead.
+                this.context.fillStyle = "black"; //Reset fade.
+                shading = 100;
+                this.context.fillText('▢', i, lyricY); //Draw the square
+                //this.context.fillRect((i / step) * blockSize, blockBaseY, 39, 39); //Draw a square
             }
         }
+    }
+
+    /**
+     * Turns timestamp into pixel based on step/block.
+     */
+    timeToPixel(time) {
+        return (time / this.step) * this.blockSize;
+    }
+
+    /**
+     * Turns timestamp into pixel based on step/block.
+     */
+    pixelToTime(pixel) {
+        return pixel / this.blockSize * this.step;
     }
 }
 
@@ -225,7 +244,6 @@ class Lyric {
         this.endTime = data.endTime;
     }
 }
-
 
 //start!
 let m = new Main();
